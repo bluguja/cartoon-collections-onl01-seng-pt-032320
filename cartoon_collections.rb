f5ac473a50d6bf51d8a1cb1866f2f87fb7cb0716
Bala Luguja <bluguja@gmail.com>
2020-03-28 20:56:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,10 +26,8 @@ def summon_captain_planet(veggies)# code an argument here
      answer
  end
 
-def find_the_cheese(cheese_arr)
-  # the array below is here to help
- # cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_arr.find do |cheese|
+def find_the_cheese(cheese_types)
+  cheese_types.find do |cheese|
     cheese_types.include?(cheese)
   end
 end
