735293a01e1fc2dc07ce59bdb1da7d8547917168
Bala Luguja <bluguja@gmail.com>
2020-03-28 19:27:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves# code an argument here
 
-  #dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
   dwarfs.each_with_index do | name , index |
     puts "#{index + 1} #{name}"
   end
