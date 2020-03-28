0fde923e7e2be66df2f82ce009d6d7e5585eb4f3
Bala Luguja <bluguja@gmail.com>
2020-03-28 20:15:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,9 @@ def roll_call_dwarves(dwarf_names)
 end
 
 def summon_captain_planet()
-  # Your code here
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+    veggies.collect do |call| 
+    call.capitalize + "!"
 end
 
 def long_planeteer_calls# code an argument here
