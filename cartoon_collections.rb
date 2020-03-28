4a74cef0e2c414780a911c8c0c2213c960df17b7
Bala Luguja <bluguja@gmail.com>
2020-03-28 19:36:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,17 +3,17 @@ def roll_call_dwarves(dwarf_names)# code an argument here
   dwarf_names.each_with_index do | d_name , index |
     p "#{index + 1} #{d_name}"
   end
-end
+# end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+# def summon_captain_planet# code an argument here
+#   # Your code here
+# end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+# def long_planeteer_calls# code an argument here
+#   # Your code here
+# end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
